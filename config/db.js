e79f5628edb11062b19902d7f378932ec32e537e
Luisa Villacorte <lufvi02@gmail.com>
2023-12-11 17:07:50 -0500

    Secondsecond part db connection

--- config/db.js
+++ config/db.js
@@ -3,10 +3,15 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
 
-        const db = await mongoose.connect('mongodb://127.0.0.1:27017/TouristPlace' , {
-useNewUrlParser: true,
-        });
+        const db = await mongoose.connect(
+            process.env.MONGO_URI, {
 
+useNewUrlParser: true,
+
+useUnifiedTopology: true,
+        });
+const url = `${db.connection.host}:${db.connection.port}`
+console.log(`MongoDB conectado en: ${url}`);
     
     } catch (error) {
         console.log('error: ${error.message}');
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
-
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
 
 const app = express();
+
+dotenv.config();
+
+connectDB();
+
+
 
 app.use('/', (req, res) => {
     res.send("HOLA MUNDO");
 });
 
+const  PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-    console.log("Server running on the port 4000");
-});
+
+app.listen(PORT, () => {
+    console.log(`Server running on the port ${PORT}`);
+}); 
